e009f763a582cb4840824a8b62fbbc720c55bcb1
Daegun Kim <blutics@gmail.com>
2023-11-01 08:05:19 +0900

    update structure

---
+++ src/front/src/js/variable.js
@@ -0,0 +1,3 @@
+const hello = "Xhello! elice! This is Sinba_d1";
+
+export default hello;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+const logger = require("./config/logger");
