5de20dade528cefb255d4d2cf490053d93630ba2
Daegun Kim <blutics@gmail.com>
2023-11-04 12:16:26 +0900

    [Back-end] no changes

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -5,12 +5,12 @@ const productsValidation = require("../../validation/products");
 const router = Router();
 router
   .get("/:id", validate(productsValidation.getProduct), (req, res) => {
-    res.json({ type: "a product" });
+    res.json({ type: "get a product" });
   })
   .get("/", validate(productsValidation.getProducts), (req, res) => {
-    res.json({ type: "a product" });
+    res.json({ type: "get products" });
   })
   .delete("/:id", validate(productsValidation.deleteProduct), (req, res) => {
-    res.json({ type: "a product" });
+    res.json({ type: "delete a product" });
   });
 module.exports = router;
