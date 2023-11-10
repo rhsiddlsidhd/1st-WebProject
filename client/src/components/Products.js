f392f0d18e6a58bb8e820fafba7c0fcd4a057152
jay <valenqbd02@gmail.com>
2023-11-10 12:26:28 +0900

    home

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -20,6 +20,7 @@ const Products = ({ products, loading, brands }) => {
       {products.map((product) => (
         <li key={product.id} className='list-group-item'>
           <span>이미지들어감</span>
+          <img src={product.main_images[0].url ?? ''} />
           <span>
             {product.title}/{product.model_number}
           </span>
